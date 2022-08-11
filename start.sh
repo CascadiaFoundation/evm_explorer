ab77a7281897eea960f43ef8229b251246615b66
Ubuntu <ubuntu@ip-172-31-90-238.ec2.internal>
2022-08-11 15:52:11 +0000

    Initial commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PATH="$PATH: /home/ubuntu/.asdf/shims/elixir"
+export SECRET_KEY_BASE=TD2OVtgG4R9ZoI9kBKHPSNViLixogJXDMV1L3l5CP0vKnfT24D3DqE/m+PBrfKET
+# export ETHEREUM_JSONRPC_HTTP_URL=https://evmexplorer.velas.com/rpc
+export ETHEREUM_JSONRPC_HTTP_URL=http://127.0.0.1:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://127.0.0.1:8546
+
+export COIN=ETH
+export SUBNETWORK=Mainnet
+export NETWORK=Ethereum
+export DATABASE_URL=postgresql://sophon:sophonpassword@localhost:5432/blockscout
+/home/ubuntu/.asdf/shims/mix phx.server
