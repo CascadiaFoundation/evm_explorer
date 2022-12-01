54eb713af3bed96bf561555e103a37d07528232c
Dev_Star <phantom93930@gmail.com>
2022-12-01 20:27:45 +0000

    change Ether to CC

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ export SECRET_KEY_BASE=TD2OVtgG4R9ZoI9kBKHPSNViLixogJXDMV1L3l5CP0vKnfT24D3DqE/m+
 export ETHEREUM_JSONRPC_HTTP_URL=http://127.0.0.1:8545
 export ETHEREUM_JSONRPC_WS_URL=ws://127.0.0.1:8546
 
-export COIN=ETH
+export COIN=CC
 export SUBNETWORK=Mainnet
-export NETWORK=Ethereum
-export DATABASE_URL=postgresql://sophon:sophonpassword@localhost:5432/blockscout
+export NETWORK=Cascadia
+export DATABASE_URL=postgresql://cascadia:cascadia@localhost:5432/blockscout
 /home/ubuntu/.asdf/shims/mix phx.server
